a08a885cd6368a0d39e5216554260c23e01f7589
vitaliy.ts <vitaliy.ts@rockalab.com>
2020-09-19 01:00:48 +0300

    first commit

---
+++ precache-manifest.056c53ca4dfaf0a71f4e444473b0caba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c78b8c5bf5a02c3526f63f23c37359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "376c2de2ffd08a6a86e4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2549e2831425e923f26",
+    "url": "/static/js/2.c48f2040.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.c48f2040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376c2de2ffd08a6a86e4",
+    "url": "/static/js/main.c6fb038a.chunk.js"
+  },
+  {
+    "revision": "2d17256c57dbe06d3a86",
+    "url": "/static/js/runtime-main.9f1d0075.js"
+  }
+]);
